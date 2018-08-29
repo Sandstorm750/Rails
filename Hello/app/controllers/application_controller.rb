bd241bd033897f7a4b346c4a990881a0452a258a
Sandstorm750 <Sandstorm750@gmail.com>
2018-08-29 23:22:31 +0500

    one

---
+++ Hello/app/controllers/application_controller.rb
@@ -0,0 +1,9 @@
+class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+  # Работа по учебнику:
+ # def hello
+  #	render text: "hello, world!"
+  #end
+
+end
